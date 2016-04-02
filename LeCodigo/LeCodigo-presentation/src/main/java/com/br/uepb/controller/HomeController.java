1725ce0433655f79251f6a15aaed305a19f7316d
Lucas Miranda Dourado <lucasmirandadourado@gmail.com>
2016-04-02 18:14:32 -0300

    Atualização do programa.

--- LeCodigo/LeCodigo-presentation/src/main/java/com/br/uepb/controller/HomeController.java
+++ LeCodigo/LeCodigo-presentation/src/main/java/com/br/uepb/controller/HomeController.java
@@ -1,55 +1,68 @@
 package com.br.uepb.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.velocity.runtime.parser.node.GetExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.br.uepb.business.PartidaBusiness;
-import com.br.uepb.domain.Partida;
+import com.br.uepb.model.LoginModel;
 
 @Controller
 public class HomeController {
-	
+
 	private static final Log LOG = LogFactory.getLog(HomeController.class);
-	
-	@Autowired
-	private PartidaBusiness partidaBusiness;
+
 
 	@RequestMapping(value = "/home/home.html", method = RequestMethod.GET)
-	public ModelAndView paginaInicialGET(HttpServletRequest request) {
-		
-		LOG.debug("Iniciada a execucao do metodo: paginaInicialGET");
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("home");
-		modelAndView.addObject("partida", new Partida(0, "hoje"));
-		
-		//request.getSession().setAttribute("lstUsers", new ArrayList<UsuarioDomain>());
-		
-		LOG.debug("Finalizada a execucao do metodo: showWelcomeHtml");
-		
-		return modelAndView;
-	}
-	
-	@RequestMapping(value = "/home/home.html", method = RequestMethod.POST)
-	public ModelAndView addNewUser(@ModelAttribute("partida") @Valid Partida partida, BindingResult bindingResult, HttpServletRequest request) {
+	public ModelAndView homeGet(HttpServletRequest request) {
+
+		LOG.debug("Iniciada a execucao do metodo: loginGet");
 
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("home");
-		
-		if(bindingResult.hasErrors()){
-			
-		}
-		
+		modelAndView.addObject("model", new LoginModel());
+		request.getSession().removeAttribute("login");
+		request.getSession().removeAttribute("idSessao");
+
+		LOG.debug("Finalizada a execucao do metodo: loginGet");
+
 		return modelAndView;
 	}
+
+	@RequestMapping(value = "/home/home.html", method = RequestMethod.POST)
+	public ModelAndView homePost(
+			@ModelAttribute("model") @Valid LoginModel model,
+			BindingResult bindingResult, HttpServletRequest request) {
+
+		LOG.debug("Iniciada a execucao do metodo: loginPost");
+
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("/home/apresentacao");
+
+		if (bindingResult.hasErrors()) {
+			modelAndView.addObject("model", new LoginModel());
+			return modelAndView;
+		}
+
+
+		LOG.debug("Finalizada a execucao do metodo: loginPost");
+		return new ModelAndView("redirect:/home/home.html");
+	}
+
 	
+	
+
 }
--- LeCodigo/LeCodigo-presentation/src/main/webapp/WEB-INF/views/home/home.jsp
+++ LeCodigo/LeCodigo-presentation/src/main/webapp/WEB-INF/views/home/home.jsp
@@ -1,16 +1,47 @@
 <!DOCTYPE html>
+<%@ include file="/WEB-INF/views/imports.jsp"%>
 <%@ include file="/WEB-INF/views/includeTags.jsp"%>
+
+<link href="styles/jogo/jogo.css" rel="stylesheet">
+<link href="../styles/jogo/fases/fase0.css" rel="stylesheet">
 <html>
 <body>
 
-	<h1>LeCODIGO</h1>
-	<br>
-	<br>
-	<h3>fazendo....</h3>
-	<br>
-	<br>
-	<h3>teste</h3>
-	<br>
-	<h3>parametro: ${partida.data_hora}</h3>
+
+
+	<div class="center-form">
+		<h1>
+			Le<br> Código
+		</h1>
+
+		<button data-original-title="Clique neste botão e irá iniciar o jogo"
+			type="button" class="btn btn-primary btn-block btn-large"
+			data-toggle="tooltip" data-placement="left" title="">Jogar</button>
+		<br>
+
+		<button
+			data-original-title="Clique neste botão e irá visualizar as instruções do jogo"
+			type="button" class="btn btn-primary btn-block" data-toggle="tooltip"
+			data-placement="left" title="">Instruções</button>
+		<br>
+
+		<button data-original-title="Clique neste botão para sair do jogo"
+			type="button" class="btn btn-primary btn-block" data-toggle="tooltip"
+			data-placement="left" title="">Sair</button>
+		<br>
+
+	</div>
+
+
+
+	<!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+	<div class="scroll-top page-scroll visible-xs visble-sm">
+		<a class="btn btn-primary" href="#page-top"><i
+			class="fa fa-chevron-up"></i></a>
+	</div>
+	<script src="../js/jogo/jogo.js"></script>
+
 </body>
+
+
 </html>
