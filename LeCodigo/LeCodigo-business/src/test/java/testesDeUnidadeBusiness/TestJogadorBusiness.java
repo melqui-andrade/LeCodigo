afdbd03c92b8417af051cc472cd63ce2909469d1
melqui-andrade <melqui.inbox@gmail.com>
2016-05-25 11:43:19 -0300

    Popular questoes funcionando a partir dos testes de unidade

--- LeCodigo/LeCodigo-business/src/test/java/testesDeUnidadeBusiness/TestJogadorBusiness.java
+++ LeCodigo/LeCodigo-business/src/test/java/testesDeUnidadeBusiness/TestJogadorBusiness.java
@@ -19,23 +19,23 @@ public class TestJogadorBusiness {
 	@Test
 	public void testCriarQuestoes(){
 		// Metodo para apagar as questoes e os bocos e criar de novo
-		QuestaoDAO questaoDAO = QuestaoDAO.getInstance();
-		questaoDAO.apagarQuestoes();
-
-		// Popular questoes da fase 1
-		PopularTabelaQuestoes popularFase1 = new PopularTabelaQuestoes();
-		popularFase1.popularQuestoes_Fase1();
-
-		// Popular questoes da fase 2
-		PopularTabelaQuestoes2 popularFase2 = new PopularTabelaQuestoes2();
-		popularFase2.popularQuestoes_Fase2();
-
-		// Popular questoes da fase 3
-		PopularTabelaQuestoes3 popularFase3 = new PopularTabelaQuestoes3();
-		popularFase3.popularQuestoes_Fase3();
-
-		// Fechar a conexão com a base de dados
-		HibernateUtil.shutdown();
+//		QuestaoDAO questaoDAO = QuestaoDAO.getInstance();
+//		questaoDAO.apagarQuestoes();
+//
+//		// Popular questoes da fase 1
+//		PopularTabelaQuestoes popularFase1 = new PopularTabelaQuestoes();
+//		popularFase1.popularQuestoes_Fase1();
+//
+//		// Popular questoes da fase 2
+//		PopularTabelaQuestoes2 popularFase2 = new PopularTabelaQuestoes2();
+//		popularFase2.popularQuestoes_Fase2();
+//
+//		// Popular questoes da fase 3
+//		PopularTabelaQuestoes3 popularFase3 = new PopularTabelaQuestoes3();
+//		popularFase3.popularQuestoes_Fase3();
+//
+//		// Fechar a conexão com a base de dados
+//		HibernateUtil.shutdown();
 		
 		assertEquals(1, 1);
 	}
